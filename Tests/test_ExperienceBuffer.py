82a79ed4b53796c0b672131fd84fad64f1095fde
Jacqueline Wegert <ja.wegert>
2021-06-18 10:33:05 +0200

    Add tests.

--- Tests/test_ExperienceBuffer.py
+++ Tests/test_ExperienceBuffer.py
@@ -3,17 +3,7 @@ from refactored.ExperienceBuffer import *
 import torch
 
 
-class TestExperienceReplay(TestCase):
-    def testStuff(self):
-        # your testcode here
-        pass
-
-    def testStuff2(self):
-        # your testcode here
-        pass
-
-
-class TestLen(TestExperienceReplay):
+class TestLen(TestCase):
     def test_len_at_initialization(self):
         experience_replay = ExperienceReplay(capacity=10)
         self.assertEqual(len(experience_replay), 0)
@@ -30,3 +20,57 @@ class TestLen(TestExperienceReplay):
             done=0.0)
         experience_replay.append(new_experience)
         self.assertEqual(len(experience_replay), 1)
+
+    def test_len_after_capacity_overflow(self):
+        experience_replay = ExperienceReplay(capacity=2)
+        new_experience = Experience(
+            state=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 2, 2, 1,
+                   0, 1, 2, 1, 2, 2, 1, 2],
+            action=torch.tensor(6),
+            reward=0.023809523809523808,
+            next_state=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 1, 0, 0, 1, 1, 2,
+                        2, 1, 1, 1, 2, 1, 2, 2, 1, 2],
+            done=0.0)
+        experience_replay.append(new_experience)
+        experience_replay.append(new_experience)
+        experience_replay.append(new_experience)
+        experience_replay.append(new_experience)
+        self.assertEqual(len(experience_replay), 2)
+
+
+class TestAppend(TestCase):
+
+    def test_append(self):
+        experience_replay = ExperienceReplay(capacity=2)
+        new_experience = Experience(
+            state=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 2, 2, 1,
+                   0, 1, 2, 1, 2, 2, 1, 2],
+            action=torch.tensor(6),
+            reward=0.023809523809523808,
+            next_state=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 1, 0, 0, 1, 1, 2,
+                        2, 1, 1, 1, 2, 1, 2, 2, 1, 2],
+            done=0.0)
+
+        new_second_experience = Experience(
+            state=[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 2, 2, 1,
+                   0, 1, 2, 1, 2, 2, 1, 2],
+            action=torch.tensor(6),
+            reward=0.023809523809523808,
+            next_state=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 1, 0, 0, 1, 1, 2,
+                        2, 1, 1, 1, 2, 1, 2, 2, 1, 2],
+            done=0.0)
+
+        new_third_experience = Experience(
+            state=[2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 2, 2, 1,
+                   0, 1, 2, 1, 2, 2, 1, 2],
+            action=torch.tensor(6),
+            reward=0.023809523809523808,
+            next_state=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 1, 0, 0, 1, 1, 2,
+                        2, 1, 1, 1, 2, 1, 2, 2, 1, 2],
+            done=0.0)
+
+        experience_replay.append(new_experience)
+        experience_replay.append(new_second_experience)
+        experience_replay.append(new_third_experience)
+
+        self.assertEqual(experience_replay.buffer[0], new_second_experience)
--- Tests/test_Trainer.py
+++ Tests/test_Trainer.py
@@ -1,9 +1,26 @@
 from unittest import TestCase
+from refactored.Trainer import Trainer
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.cuda
 
+class TestChangeReward(TestCase):
 
-class TestTrainer(TestCase):
-    def test_change_reward(self):
-        pass
+    def test_change_reward_with_reward_1_and_done_true(self):
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        trainer = Trainer(hidden_dim=10, buffer_size=2, gamma=0.8, batch_size=32, device=device)
+        self.assertEqual(trainer.change_reward(reward=1, done=True, board=None), 10)
+
+    def test_change_reward_with_reward_None_and_done_true(self):
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        trainer = Trainer(hidden_dim=10, buffer_size=2, gamma=0.8, batch_size=32, device=device)
+        self.assertEqual(trainer.change_reward(reward=-1, done=True, board=None), -10)
+
+    def test_change_reward_with_reward_minus_1_and_done_true(self):
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        trainer = Trainer(hidden_dim=10, buffer_size=2, gamma=0.8, batch_size=32, device=device)
+        self.assertEqual(trainer.change_reward(reward=None, done=True, board=None), -20)
+
 
     def test_take_action(self):
         pass
